fc0a6d927e67f977e1785db300d0a7e737a951ad
KorieDrakeChaney <KorieDrakeChaney@gmail.com>
2024-02-10 23:45:51 -0600

    Update color_command.rs

--- src/section/events/command/color_command.rs
+++ src/section/events/command/color_command.rs
@@ -21,13 +21,6 @@ impl std::fmt::Display for ColorCommand {
 
 impl ColorCommand {
     pub fn parse(s: &[&str]) -> std::io::Result<Self> {
-        if s.len() < 9 {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                format!("Invalid length of {}, needs 9", s.len()),
-            ));
-        }
-
         let easing = match s[0].parse() {
             Ok(x) => x,
             Err(_) => {
@@ -48,36 +41,48 @@ impl ColorCommand {
             }
         };
 
-        let end_time = match s[2].parse() {
-            Ok(x) => x,
-            Err(_) => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    format!("Invalid Color Command token: {}", s[2]),
-                ))
+        let has_end_color = (s.len() == 6 && s[s.len() - 4].is_empty()) || s.len() == 9;
+
+        let end_color = if let (Some(r), Some(g), Some(b)) = (
+            s[s.len() - 3].parse().ok(),
+            s[s.len() - 2].parse().ok(),
+            s[s.len() - 1].parse().ok(),
+        ) {
+            Color::new(r, g, b)
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid Color Command Data",
+            ));
+        };
+
+        let start_color = if s[s.len() - 4].is_empty() || !has_end_color {
+            end_color.clone()
+        } else if let (Some(r), Some(g), Some(b)) = (
+            s[s.len() - 4].parse().ok(),
+            s[s.len() - 3].parse().ok(),
+            s[s.len() - 2].parse().ok(),
+        ) {
+            Color::new(r, g, b)
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Invalid Color Command Data",
+            ));
+        };
+
+        let end_time = if s.len() == 6 && has_end_color {
+            start_time
+        } else {
+            match s[2].parse() {
+                Ok(x) => x,
+                Err(_) => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidInput,
+                        format!("Invalid Color Command token: {}", s[2]),
+                    ))
+                }
             }
-        };
-
-        let end_color = if let (Some(r), Some(g), Some(b)) =
-            (s[8].parse().ok(), s[9].parse().ok(), s[10].parse().ok())
-        {
-            Color::new(r, g, b)
-        } else {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                "Invalid Color Command Data",
-            ));
-        };
-
-        let start_color = if let (Some(r), Some(g), Some(b)) =
-            (s[5].parse().ok(), s[6].parse().ok(), s[7].parse().ok())
-        {
-            Color::new(r, g, b)
-        } else {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                "Invalid Color Command Data",
-            ));
         };
 
         Ok(ColorCommand {
